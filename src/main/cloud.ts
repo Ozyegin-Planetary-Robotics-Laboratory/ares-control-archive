fd43c456eda66c110493d130873f348256aa4dc6
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-06-04 15:44:34 +0000

    Modified cloud.ts

--- src/main/cloud.ts
+++ src/main/cloud.ts
@@ -2,15 +2,11 @@ import * as THREE from 'three'
 import { Buffer } from 'buffer'
 
 export function parse(msg): THREE.Points {
-
   const buffer = Buffer.from(msg.data)
-  const xyz = new Float32Array(buffer.buffer, buffer.byteOffset, buffer.byteLength / 4)
-  console.log(xyz)
-
+  const xyz_data = new Float32Array(buffer.buffer, buffer.byteOffset, buffer.byteLength / 4)
   const geometry = new THREE.BufferGeometry()
-  geometry.setAttribute('position', new THREE.BufferAttribute(xyz, 3))
+  geometry.setAttribute('position', new THREE.BufferAttribute(xyz_data, 3))
   const material = new THREE.PointsMaterial({ size: 0.1, color: 0xff0000 })
   const mesh = new THREE.Points(geometry, material)
-
   return mesh
 }
